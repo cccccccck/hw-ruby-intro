2dc035953ceae7514287ea53541f7f5a1e3b811e
Chu Ke <sd_chuke@outlook.com>
2018-05-22 07:08:09 +0000

    commit info

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,75 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  s=0
+  arr.each do |i|
+    s+=i;
+  end
+  s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.size==1
+  arr=arr.sort
+  arr[arr.size-2]+arr[arr.size-1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty?
+  return false if arr.size==1
+  i=0
+  j=0
+  while i < (arr.size-1) 
+   a=arr[i]
+   j=i+1
+    while j<arr.size
+        b=arr[j]
+        return true if a+b==n
+        j=j+1
+    end
+      i=i+1
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s==""
+    return false
+  elsif s =~ /^[^a-zA-Z](.*)/
+    return false
+  elsif s=~ /^[aeiouAEIOU](.*)/
+    return false
+  end
+  true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s =~ /^[01]+$/
+    s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+def initialize(isbn, price)
+      raise ArgumentError if isbn.empty?
+      raise ArgumentError if price <= 0
+      @isbn = isbn
+      @price = price
+end
+  attr_accessor :isbn
+  attr_accessor :price
+  def price_as_string
+      "$#{sprintf("%.2f", @price)}"
+  end
+
 end
